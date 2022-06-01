c01572f8812c66de0e207a809daa37c65f9ee7d7
Do Duc Anh <doducanh0312@gmail.com>
2022-06-01 17:32:47 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <div className=" text-red-600">con cu</div>
+    </div>
+  );
+}
+
+export default App;
